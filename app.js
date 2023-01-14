122f747ee0b47f6e05b9a537f2509e2874480112
mingin2007 <mingin2007@gmail.com>
2023-01-14 02:17:58 +0200

    34

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.set("view engine", "ejs");
 let files = fs.readdirSync('./JSON/', {encoding: "utf-8"});
 console.log(files);
 
+app.get('/', (req, res)=>{
+    res.send(files);
+    res.end();
+    console.log("res send");
+});
+
 app.get("/test", (req, res) => {
     res.render("index", {files: files}); // index refers to index.ejs
    });
